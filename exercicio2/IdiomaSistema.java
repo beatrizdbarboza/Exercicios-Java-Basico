d35be454b363370ef2377c9a7aa84456cde8c209
Beatriz Dias <beatrizzbarbozaa88@gmail.com>
2026-08-25 14:30:55 -0300

    Create IdiomaSistema.java

---
+++ exercicio2/IdiomaSistema.java
@@ -0,0 +1,11 @@
+package idiomasistema;
+
+import java.util.Locale;
+
+public class IdiomaSistema {
+    public static void main(String[] args){
+        Locale idioma = Locale.getDefault();
+        System.out.println("O idioma do sistema está em:");
+        System.out.println(idioma);
+    }
+}
