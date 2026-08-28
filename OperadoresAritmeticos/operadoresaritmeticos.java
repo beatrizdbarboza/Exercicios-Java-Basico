3f37ae89351f343f30684e38d8d2c09c5cde78fb
Beatriz Dias <beatrizzbarbozaa88@gmail.com>
2026-08-28 13:23:56 -0300

    Update operadoresaritmeticos.java

--- OperadoresAritmeticos/operadoresaritmeticos.java
+++ OperadoresAritmeticos/operadoresaritmeticos.java
@@ -19,6 +19,35 @@ public class OperadoresAritmeticos {
         int n2 = 5;
         float media = (n1 + n2) / 2;
         System.out.println("A média é igual a " + media);
+        
+        System.out.println("----------------");
+        
+        int numero = 5;
+        int valor = 5 + ++numero;
+        System.out.println(valor);
+        
+        System.out.println("----------------");
+        
+        int x = 4;
+        x += 2;
+        System.out.println(x);
+        
+        System.out.println("----------------");
+        
+        float raiz = (float) Math.sqrt(x);
+        System.out.println(raiz);
+        
+        System.out.println("----------------");
+        
+        float v = 8.9f;
+        int ar = (int) Math.floor(v);
+        System.out.println(ar);
+
+        System.out.println("----------------");
+
+        double ale = Math.random();
+        int n = (int) (5 + ale * (10 - 5));
+        System.out.println(n);
     }
     
 }
